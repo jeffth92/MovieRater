58659fd382b7a36011b96c9979a49ea3b6178fd8
Colin Koestler <KoestlerColinR@gmail.com>
2021-08-31 21:38:12 -0400

    Built services mostly and Controller Completely

--- MovieRater.Services/RatingService.cs
+++ MovieRater.Services/RatingService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MovieRater.Data;
+using MovieRater.Models.RatingModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,68 @@ using System.Threading.Tasks;
 
 namespace MovieRater.Services
 {
-    class RatingService
+    public class RatingService
     {
+  private readonly Guid _userId;
+
+        public RatingService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateMovieRating(CreateMovieRating model)
+        {
+            var entity =
+                new Rating()
+                {
+                    OwnerID = _userId,
+                    MovieID = model.MovieID,
+                    RatingStar = model.RatingStar,
+                    Comment = model.Comment
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Ratings.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public bool CreateShowRating(CreateShowRating model)
+        {
+            var entity =
+                new Rating()
+                {
+                    OwnerID = _userId,
+                    ShowID = model.ShowID,
+                    RatingStar = model.RatingStar,
+                    Comment = model.Comment
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Ratings.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<RatingListItem> GetRatingsByMovieID(int movieId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Ratings
+                        .Where(e => e.MovieID == movieId)
+                        .Select(
+                            e =>
+                                new RatingListItem
+                                {
+                                    MovieID = e.MovieID,
+                                    Ratings = new List<Rating>(),
+                                    Title = e.Movie.Title
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
     }
 }
--- MovieRaterAPI/Controllers/RatingController.cs
+++ MovieRaterAPI/Controllers/RatingController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using MovieRater.Models.RatingModels;
+using MovieRater.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,41 @@ using System.Web.Http;
 
 namespace MovieRaterAPI.Controllers
 {
+    [Authorize]
     public class RatingController : ApiController
     {
+        private RatingService CreateRatingService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var ratingService = new RatingService(userId);
+            return ratingService;
+        }
+        [Route("api/rating/{movieID}")]
+        public IHttpActionResult GetMovie(int movieID)
+        {
+            RatingService ratingService = CreateRatingService();
+            var rating = ratingService.GetRatingsByMovieID(movieID);
+            return Ok(rating);
+        }
+        [Route("api/rating/{showID}")]
+        public IHttpActionResult GetShow(int showID)
+        {
+            RatingService ratingService = CreateRatingService();
+            var rating = ratingService.GetRatingsByShowID(showID);
+            return Ok(rating);
+        }
+
+        public IHttpActionResult Post(CreateMovieRating rating)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateRatingService();
+
+            if (!service.CreateRating(rating))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
