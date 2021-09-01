4e1a0a51e874f6baf1f85dcbe092164a5becd581
thatseanmoore <spmoore19@gmail.com>
2021-08-31 21:15:10 -0400

    First commit

--- MovieRater.Services/MovieService.cs
+++ MovieRater.Services/MovieService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MovieRater.Data;
+using MovieRater.Models.MovieModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,47 @@ using System.Threading.Tasks;
 
 namespace MovieRater.Services
 {
-    class MovieService
+    public class MovieService
     {
+        private readonly Guid _userId;
+        public MovieService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateMovie(MovieCreate model)
+        {
+            var entity =
+                new Movie()
+                {
+                    OwnerID = _userId,
+                    Title = model.Title,
+                    Description = model.Description,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Movies.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<MovieListItem> GetAllMovies()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Movies
+                    .Where(e => e.OwnerID == _userId)
+                    .Select(
+                        e =>
+                        new MovieListItem
+                        {
+                            Id = e.MovieID,
+                            Title = e.Title,
+                        }
+                );
+
+                return query.ToArray();
+            }
+        }
     }
 }
--- MovieRaterAPI/Controllers/MovieController.cs
+++ MovieRaterAPI/Controllers/MovieController.cs
@@ -4,10 +4,39 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MovieRater.Services;
+using MovieRater.Models;
+using MovieRater.Models.MovieModels;
+using Microsoft.AspNet.Identity;
 
 namespace MovieRaterAPI.Controllers
 {
+    [Authorize]
     public class MovieController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            MovieService movieService = CreateMovieService();
+            var movie = movieService.GetAllMovies();
+            return Ok(movie);
+        }
+        public IHttpActionResult Movie(MovieCreate movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateMovieService();
+
+            if (!service.CreateMovie(movie))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private MovieService CreateMovieService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var movieService = new MovieService(userId);
+            return movieService;
+        }
     }
 }
